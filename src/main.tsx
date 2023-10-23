e17bd7d6337b2f8e56d4225d3c987aa9ca1f31e8
Augusto <ampiatto@hotmail.com>
2023-10-23 06:41:57 -0300

    tentativa de instalar tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./assets/styles/scss.scss";
+import "./assets/styles/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
