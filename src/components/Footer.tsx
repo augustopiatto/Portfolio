c0f82afdba8c44486470255fafd81e1ba35f4ae7
augustopiatto <ampiatto@hotmail.com>
2024-01-12 18:54:42 -0300

    ajuste de header e footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className="bg-gradient-to-b from-title-lighter to-30% to-paragraph to-100% flex flex-col gap-5 px-5 py-10 medium:px-40 medium:flex-row medium:items-center">
       <div className="flex flex-1 justify-center">
-        <p className="font-chivo text-2xl text-secondary">
+        <p className="font-chivo text-secondary text-lg medium:text-2xl">
           Sinta-se à vontade para entrar em{" "}
           <span className="text-highlight font-bold">contato</span> comigo.
           Vamos bater um papo!
