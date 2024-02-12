0d567f0c403f955e054f3f03bc43c145369eb5dc
augustopiatto <ampiatto@hotmail.com>
2024-02-12 19:26:39 -0300

    dialog button

--- src/components/HTMLComponents/button/Button.stories.tsx
+++ src/components/HTMLComponents/button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Button } from "./Button";
+import { Button, ButtonInterface } from "./Button";
+import expansionRightWhite from "/svg/expansion-right-white.svg";
 
 export default {
   title: "Components/Button",
@@ -18,3 +19,15 @@ export const Default: Story = {
     ),
   },
 };
+
+export const Dialog = (args: ButtonInterface) => (
+  <div className="absolute cursor-pointer h-20 left-5 w-20 [&>*]:w-10 [&>*]:w-20 [&>*]:px-0 [&>*]:py-0 [&>*]:h-full [&>*]:rounded-bl-2xl [&>*]:rounded-br-none [&>*]:rounded-t-none [&>button]:bg-highlight [&>button]:shadow-none [&>button]:border-none [&>button]:hover:bg-darker-highlight [&>button]:hover:scale-105">
+    <Button {...args}>
+      <img
+        src={expansionRightWhite}
+        alt="expansion-right"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-4 w-6 -rotate-90 visible"
+      />
+    </Button>
+  </div>
+);
